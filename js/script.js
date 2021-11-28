ef7f36446aada791196b43037725fb59f9514208
GraceMwende <grace.micheni@student.moringaschool.com>
2021-11-28 23:30:16 +0300

    update price

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ function Pizza(size, toppings, pizzaPrice) {
   this.pizzaPrice = 0;
 }
 
+Pizza.prototype.setToppings = function (toppings) {
+  this.toppings = toppings;
+}
+
+Pizza.prototype.setSize = function (size) {
+  this.size = size;
+}
+
 Pizza.prototype.price = function () {
   if (this.size === "small") {
     this.pizzaPrice = 200;
@@ -15,83 +23,74 @@ Pizza.prototype.price = function () {
     this.pizzaPrice = 600;
   }
 
-  if (this.toppings.indexOf("sausage") >= 0) {
-    this.pizzaPrice += 40;
-  }
-  if (this.toppings.indexOf("cheese") >= 0) {
-    this.pizzaPrice += 50;
-  }
-  if (this.toppings.indexOf("mushroom") >= 0) {
-    this.pizzaPrice += 60;
-  }
-  if (this.toppings.indexOf("olives") >= 0) {
-    this.pizzaPrice += 70;
-  }
-  if (this.toppings.indexOf("pepper") >= 0) {
-    this.pizzaPrice += 80;
+  if (this.toppings) {
+    if (this.toppings.indexOf("sausage") >= 0) {
+      this.pizzaPrice += 40;
+    }
+    if (this.toppings.indexOf("cheese") >= 0) {
+      this.pizzaPrice += 50;
+    }
+    if (this.toppings.indexOf("mushroom") >= 0) {
+      this.pizzaPrice += 60;
+    }
+    if (this.toppings.indexOf("olives") >= 0) {
+      this.pizzaPrice += 70;
+    }
+    if (this.toppings.indexOf("pepper") >= 0) {
+      this.pizzaPrice += 80;
+    }
   }
 
   return this.pizzaPrice;
 }
 
 $(document).ready(function () {
-  $(".modal-body").submit(function (event) {
-    event.preventDefault();
+  var size = $("input[type = radio][name = size]:checked").val();
 
-    var size = $("input[type = radio][name = size]:checked").val();
-    var toppings = $("input[type=checkbox][name=toppings]:checked");
-    var i = 0;
-    var toppingVal;
-    while (i < toppings.length) {
-      toppingVal += " " + (toppings[i]).value;
-      i++;
-    }
-
-    var newPizza = new Pizza(size, toppingVal, pizzaPrice);
-    newPizza.price();
-
-    $("#responses").append("<li>" + "You ordered a " + newPizza.size + " " + newPizza.toppings + " pizza. " + " Your total price is " + newPizza.pizzaPrice + "</li>")
-  });
-});
-
-// js cart plugin
-$(function () {
-
-  var goToCartIcon = function ($addTocartBtn) {
-    var $cartIcon = $(".my-cart-icon");
-    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({
-      "position": "fixed",
-      "z-index": "999"
-    });
-    $addTocartBtn.prepend($image);
-    var position = $cartIcon.position();
-    $image.animate({
-      top: position.top,
-      left: position.left
-    }, 500, "linear", function () {
-      $image.remove();
-    });
+  var toppings = $("input[type=checkbox][name=toppings]:checked");
+  var i = 0;
+  var toppingVal;
+  while (i < toppings.length) {
+    toppingVal += " " + (toppings[i]).value;
+    i++;
   }
 
-  $('.my-cart-btn').myCart({
-    classCartIcon: 'my-cart-icon',
-    classCartBadge: 'my-cart-badge',
-    affixCartIcon: true,
-    checkoutCart: function (products) {
-      $.each(products, function () {
-        console.log(this);
-      });
-    },
-    clickOnAddToCart: function ($addTocart) {
-      goToCartIcon($addTocart);
-    },
-    getDiscountPrice: function (products) {
-      var total = 0;
-      $.each(products, function () {
-        total += this.quantity * this.price;
-      });
-      return total * 0.5;
-    }
-  });
+  var newPizza = new Pizza(size, toppingVal, pizzaPrice);
+  // newPizza.price();
 
+  $("input[type = radio][name = size]").on("change", function () {
+    newPizza.setSize(getSize());
+    newPizza.price();
+    updatePrice(newPizza);
+  })
+
+  $("input[type='checkbox'][name='toppings']").on("change", function () {
+    newPizza.setToppings(getToppings());
+    newPizza.price();
+    updatePrice(newPizza);
+    console.log(newPizza);
+  })
+  // Add to cart btn
+  $(".my-cart-btn").click(function (event) {
+    event.preventDefault();
+  });
 });
+
+function getToppings() {
+  var toppings = $("input[type=checkbox][name=toppings]:checked");
+  var i = 0;
+  var toppingVal;
+  while (i < toppings.length) {
+    toppingVal += " " + (toppings[i]).value;
+    i++;
+  }
+  return toppingVal;
+}
+
+function getSize() {
+  return $("input[type = radio][name = size]:checked").val();
+}
+
+function updatePrice(newPizza) {
+  $("#responses").html("<li>" + " Your total price is " + newPizza.pizzaPrice + "</li>")
+}
